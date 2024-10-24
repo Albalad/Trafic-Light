e7edf988170cf05f4356caefb5e96a146e269d35
Alvaro <alvaro.albalad@gmail.com>
2024-10-24 15:33:36 +0000

    proyecto completado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -58,13 +58,17 @@ const Home = () => {
 	return (
 		<div>
 			<div>
+
 				<div className="paloSemaforo"></div>
 				<div className="luces">
+
 					<div className={`luzRoja ${roja}`}><button className="luzRoja" onClick={() => { encendidoRojo() }}></button></div>
 					<div className={`luzAmarilla ${naranja}`}><button className="luzAmarilla" onClick={() => { encendidoNaranja() }}></button></div>
 					<div className={`luzVerde ${verde}`}><button className="luzVerde" onClick={() => { encendidoVerde() }}></button></div>
 					<div className={`${oculto} morado`}><button className="morado"></button></div>
+                    
 				</div>
+
 			</div>
 			<div className="botonCambio">
 				<button className="cambioDeLuz" onClick={() => intercambio()}>Cambia las luces</button>
